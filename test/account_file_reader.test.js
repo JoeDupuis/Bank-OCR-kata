9ef1d0e9c8c8c2e637030a44932d4006d12bc56c
Joe Dupuis <joe@dupuis.io>
2020-07-26 17:52:37 -0700

    AccountFileReader#resolveDigit

--- test/account_file_reader.test.js
+++ test/account_file_reader.test.js
@@ -49,4 +49,93 @@ describe('AccountFileReader', ()=>{
 			expect(digits[8]).toBe(FIXTURE_DIGIT_0)
 		})
 	})
+
+	describe('Should resolve scanned digit to a number', ()=>{
+		test('Resolve 0', ()=>{
+			let digitToResolve = FIXTURE_DIGIT_0
+
+			let digit = subject.resolveDigit(digitToResolve)
+
+			expect(digit).toBe(0)
+		})
+
+		test('Resolve 1', ()=>{
+			let digitToResolve = FIXTURE_DIGIT_1
+
+			let digit = subject.resolveDigit(digitToResolve)
+
+			expect(digit).toBe(1)
+		})
+
+		test('Resolve 2', ()=>{
+			let digitToResolve = FIXTURE_DIGIT_2
+
+			let digit = subject.resolveDigit(digitToResolve)
+
+			expect(digit).toBe(2)
+		})
+
+		test('Resolve 3', ()=>{
+			let digitToResolve = FIXTURE_DIGIT_3
+
+			let digit = subject.resolveDigit(digitToResolve)
+
+			expect(digit).toBe(3)
+		})
+
+
+		test('Resolve 4', ()=>{
+			let digitToResolve = FIXTURE_DIGIT_4
+
+			let digit = subject.resolveDigit(digitToResolve)
+
+			expect(digit).toBe(4)
+		})
+
+
+		test('Resolve 5', ()=>{
+			let digitToResolve = FIXTURE_DIGIT_5
+
+			let digit = subject.resolveDigit(digitToResolve)
+
+			expect(digit).toBe(5)
+		})
+
+
+		test('Resolve 6', ()=>{
+			let digitToResolve = FIXTURE_DIGIT_6
+
+			let digit = subject.resolveDigit(digitToResolve)
+
+			expect(digit).toBe(6)
+		})
+
+
+		test('Resolve 7', ()=>{
+			let digitToResolve = FIXTURE_DIGIT_7
+
+			let digit = subject.resolveDigit(digitToResolve)
+
+			expect(digit).toBe(7)
+		})
+
+
+		test('Resolve 8', ()=>{
+			let digitToResolve = FIXTURE_DIGIT_8
+
+			let digit = subject.resolveDigit(digitToResolve)
+
+			expect(digit).toBe(8)
+		})
+
+
+		test('Resolve 9', ()=>{
+			let digitToResolve = FIXTURE_DIGIT_9
+
+			let digit = subject.resolveDigit(digitToResolve)
+
+			expect(digit).toBe(9)
+		})
+
+	})
 })
